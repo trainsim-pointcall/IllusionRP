d8283aa2e4d4f9376af15db5e0817fb842a01773
AkiKurisu <2683987717@qq.com>
2026-06-29 22:24:05 +0800

    Fix motion vector debug render graph output

--- Runtime/RenderPipeline/PostProcessing/MotionVectorsDebugPass.cs
+++ Runtime/RenderPipeline/PostProcessing/MotionVectorsDebugPass.cs
@@ -1,6 +1,7 @@
 ﻿#if DEVELOPMENT_BUILD || UNITY_EDITOR
 using System;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
@@ -14,7 +15,7 @@ namespace Illusion.Rendering
         public MotionVectorsDebugPass(IllusionRendererData rendererData)
         {
             profilingSampler = new ProfilingSampler("Motion Vectors Debug");
-            renderPassEvent = IllusionRenderPassEvent.MotionVectorDebugPass;
+            renderPassEvent = IllusionRenderPassEvent.FullScreenDebugPass;
             ConfigureInput(ScriptableRenderPassInput.Motion);
         }
         
@@ -22,35 +23,67 @@ namespace Illusion.Rendering
         {
             internal Material MotionVectorDebugMaterial;
             internal TextureHandle MotionVectorColor;
-            internal TextureHandle ColorDestination;
+            internal Vector4 DebugParams;
+        }
+
+        private class FinalBlitPassData
+        {
+            internal TextureHandle Source;
         }
 
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
         {
             var resources = frameData.Get<UniversalResourceData>();
             var motionVectorFromResources = resources.motionVectorColor;
-            if (!motionVectorFromResources.IsValid()) return;
-
+            TextureHandle colorTargetHandle = resources.cameraColor;
+            var debugOutputDesc = renderGraph.GetTextureDesc(colorTargetHandle);
+            debugOutputDesc.name = "Motion Vectors Debug Output";
+            debugOutputDesc.msaaSamples = MSAASamples.None;
+            debugOutputDesc.depthBufferBits = DepthBits.None;
+            debugOutputDesc.clearBuffer = false;
+            TextureHandle debugOutput = renderGraph.CreateTexture(debugOutputDesc);
             
-            TextureHandle colorTargetHandle = resources.activeColorTexture;
-            
-            using (var builder = renderGraph.AddRasterRenderPass<MotionVectorsDebugPassData>("Motion Vectors Debug", 
+            using (var builder = renderGraph.AddRasterRenderPass<MotionVectorsDebugPassData>("Motion Vectors Debug",
                 out var passData, profilingSampler))
             {
                 passData.MotionVectorDebugMaterial = _motionVectorDebugMaterial.Value;
-                builder.UseTexture(motionVectorFromResources);
-                passData.MotionVectorColor = motionVectorFromResources;
+                passData.DebugParams = new Vector4(
+                    motionVectorFromResources.IsValid() ? 1.0f : 0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f);
+                if (motionVectorFromResources.IsValid())
+                {
+                    builder.UseTexture(motionVectorFromResources);
+                    passData.MotionVectorColor = motionVectorFromResources;
+                }
                 
-                builder.SetRenderAttachment(colorTargetHandle, 0);
-                passData.ColorDestination = colorTargetHandle;
+                builder.SetRenderAttachment(debugOutput, 0);
                 
                 builder.AllowPassCulling(false);
                 builder.AllowGlobalStateModification(true);
                 
                 builder.SetRenderFunc(static (MotionVectorsDebugPassData data, RasterGraphContext context) =>
                 {
-                    data.MotionVectorDebugMaterial.SetTexture(IllusionShaderProperties._MotionVectorTexture, data.MotionVectorColor);
-                    Blitter.BlitTexture(context.cmd, new Vector4(1, 1, 0, 0), data.MotionVectorDebugMaterial, 0);
+                    if (data.MotionVectorColor.IsValid())
+                        data.MotionVectorDebugMaterial.SetTexture(IllusionShaderProperties._MotionVectorTexture, data.MotionVectorColor);
+                    data.MotionVectorDebugMaterial.SetVector("_DebugMotionVectorsParams", data.DebugParams);
+                    context.cmd.DrawProcedural(Matrix4x4.identity, data.MotionVectorDebugMaterial, 0,
+                        MeshTopology.Triangles, 3, 1);
+                });
+            }
+
+            using (var builder = renderGraph.AddRasterRenderPass<FinalBlitPassData>("Motion Vectors Debug Final Blit",
+                out var passData, profilingSampler))
+            {
+                builder.UseTexture(debugOutput);
+                passData.Source = debugOutput;
+                builder.SetRenderAttachment(colorTargetHandle, 0);
+                builder.AllowPassCulling(false);
+
+                builder.SetRenderFunc(static (FinalBlitPassData data, RasterGraphContext context) =>
+                {
+                    Blitter.BlitTexture(context.cmd, data.Source, new Vector4(1, 1, 0, 0), 0.0f, false);
                 });
             }
         }
--- Shaders/PostProcessing/DebugMotionVectors.shader
+++ Shaders/PostProcessing/DebugMotionVectors.shader
@@ -1,53 +1,66 @@
 ﻿Shader "Hidden/DebugMotionVectors"
 {
+    HLSLINCLUDE
+    #include "Packages/com.kurisu.illusion-render-pipelines/ShaderLibrary/ShaderVariables.hlsl"
+    #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+
+    #pragma vertex Vert
+    #pragma fragment Frag
+
+    TEXTURE2D_X(_MotionVectorTexture);
+    SAMPLER(sampler_MotionVectorTexture);
+
+    float4 _DebugMotionVectorsParams;
+    #define _DebugMotionVectorValid _DebugMotionVectorsParams.x
+
+    struct Attributes
+    {
+        uint vertexID : SV_VertexID;
+        UNITY_VERTEX_INPUT_INSTANCE_ID
+    };
+
+    struct Varyings
+    {
+        float4 positionCS : SV_POSITION;
+        float2 texcoord : TEXCOORD0;
+        UNITY_VERTEX_OUTPUT_STEREO
+    };
+
+    Varyings Vert(Attributes input)
+    {
+        Varyings output;
+        UNITY_SETUP_INSTANCE_ID(input);
+        UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+        output.positionCS = GetFullScreenTriangleVertexPosition(input.vertexID);
+        output.texcoord = GetNormalizedFullScreenTriangleTexCoord(input.vertexID);
+        return output;
+    }
+    ENDHLSL
+
     SubShader
     {
         Tags
         {
             "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline"
         }
+
         Pass
         {
             ZTest Always
             Cull Off
             ZWrite Off
-            
+
             HLSLPROGRAM
-            #pragma vertex vert
-            #pragma fragment frag
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-
-            TEXTURE2D_X(_MotionVectorTexture);
-            SAMPLER(sampler_MotionVectorTexture);
-
-            struct Attributes
+            half4 Frag(Varyings input) : SV_Target
             {
-                float4 positionOS : POSITION;
-                float2 uv         : TEXCOORD0;
-            };
+                if (_DebugMotionVectorValid < 0.5f)
+                    return half4(0.8, 0.0, 0.8, 1.0);
 
-            struct Varyings
-            {
-                float4 positionHCS : SV_POSITION;
-                float2 uv          : TEXCOORD0;
-            };
+                float2 motion = SAMPLE_TEXTURE2D_X(_MotionVectorTexture, sampler_MotionVectorTexture, input.texcoord).xy;
 
-            Varyings vert(Attributes IN)
-            {
-                Varyings OUT;
-                OUT.positionHCS = TransformObjectToHClip(IN.positionOS.xyz);
-                OUT.uv = IN.uv;
-                return OUT;
-            }
-
-            half4 frag(Varyings IN) : SV_Target
-            {
-                float2 motion = SAMPLE_TEXTURE2D_X(_MotionVectorTexture, sampler_MotionVectorTexture, IN.uv).xy;
-
-                // direction -> color, length -> intensity
-                float mag = length(motion) * 10.0;
-                float angle = atan2(motion.y, motion.x) / 3.14159;
-                float3 color = 0.5 + 0.5 * cos(float3(0, 2.094, 4.188) + angle * 6.283);
+                float mag = length(motion) * 10.0f;
+                float angle = atan2(motion.y, motion.x) / 3.14159f;
+                float3 color = 0.5f + 0.5f * cos(float3(0, 2.094f, 4.188f) + angle * 6.283f);
                 return half4(color * saturate(mag), 1.0);
             }
             ENDHLSL
